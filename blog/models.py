03a3c5bfe4670b82ce549b57b3b7c944b978cd41
Gennady <netbooks@mail.ru>
2020-12-16 23:59:43 +0300

    Create models for project (Post, Category, Tag)

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    title = models.CharField(max_length=255, verbose_name='Наименование')
+    slug = models.SlugField(max_length=255, verbose_name='Url', unique=True)
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        ordering = ['title']
+
+
+class Tag(models.Model):
+    title = models.CharField(max_length=60, verbose_name='Наименование')
+    slug = models.SlugField(max_length=50, verbose_name='Url', unique=True)
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        ordering = ['title']
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255, verbose_name='Наименование')
+    slug = models.SlugField(max_length=255, verbose_name='Url', unique=True)
+    author = models.CharField(max_length=100, verbose_name='Автор')
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Опубликовано')
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True, verbose_name='Фото')
+    views = models.IntegerField(default=0, verbose_name='Кол-во просмотров')
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name='posts')
+    tags = models.ManyToManyField(Tag, blank=True, related_name='posts')
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        ordering = ['-created_at']
+
+
+
