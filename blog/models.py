287f1482b7c95aa147397ae3e6238b21baee5087
Gennady <netbooks@mail.ru>
2020-12-18 16:07:09 +0300

    Create header and footer from models Category

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 class Category(models.Model):
     title = models.CharField(max_length=255, verbose_name='Наименование')
@@ -6,6 +7,9 @@ class Category(models.Model):
 
     def __str__(self):
         return self.title
+    
+    def get_absolute_url(self):
+        return reverse('category', kwargs={"slug": self.slug})
     
     class Meta:
         verbose_name = 'Категория(ю)'
--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import index
+from .views import index, get_category
 
 urlpatterns = [
     path('', index, name='index'),
+    path('category/<str:slug>', get_category, name='category'),
 ]
