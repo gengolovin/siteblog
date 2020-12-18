5dba6f78c9c591961d5d812318fdd9173e3890df
Gennady <netbooks@mail.ru>
2020-12-18 20:52:21 +0300

    Modify active.js for header blog

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,5 @@ from .views import index, get_category
 
 urlpatterns = [
     path('', index, name='index'),
-    path('category/<str:slug>', get_category, name='category'),
+    path('category/<str:slug>/', get_category, name='category'),
 ]
